800fe3825d5dedd0dab42a76897d8ad3bbda9acd
Irvin Velarde <irvinvelarde@gmail.com>
2018-06-03 10:58:19 -0500

    Modificando Vistas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,11 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-primary">
-                <div class="panel-heading">Bienvinido al Portal de Incidencias</div>
+                <div class="panel-heading">Bienvenido al Portal de Incidencias</div>
                 <div class="panel-body">
                     <p style="text-align: justify;">        
+                        Para mejorar la efectividad en la comunicación con el cliente a la hora de registrar una incidencia y realizar el seguimiento de la resolución, hemos creado el  sistema TTiPo, un portal accesible vía Internet, que permite de forma remota dar de alta incidencias y peticiones, realizar consultas, modificar un boletín y seguir su histórico.
+                        Para mejorar la efectividad en la comunicación con el cliente a la hora de registrar una incidencia y realizar el seguimiento de la resolución, hemos creado el  sistema TTiPo, un portal accesible vía Internet, que permite de forma remota dar de alta incidencias y peticiones, realizar consultas, modificar un boletín y seguir su histórico.
                         Para mejorar la efectividad en la comunicación con el cliente a la hora de registrar una incidencia y realizar el seguimiento de la resolución, hemos creado el  sistema TTiPo, un portal accesible vía Internet, que permite de forma remota dar de alta incidencias y peticiones, realizar consultas, modificar un boletín y seguir su histórico.
                         Para mejorar la efectividad en la comunicación con el cliente a la hora de registrar una incidencia y realizar el seguimiento de la resolución, hemos creado el  sistema TTiPo, un portal accesible vía Internet, que permite de forma remota dar de alta incidencias y peticiones, realizar consultas, modificar un boletín y seguir su histórico.
                         Para mejorar la efectividad en la comunicación con el cliente a la hora de registrar una incidencia y realizar el seguimiento de la resolución, hemos creado el  sistema TTiPo, un portal accesible vía Internet, que permite de forma remota dar de alta incidencias y peticiones, realizar consultas, modificar un boletín y seguir su histórico.
